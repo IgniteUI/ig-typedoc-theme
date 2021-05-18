18e49e5627bc0f93bd129b5ba483ab8ccad7e2da
Aleksandyr <aleksandyr.kamenov@gmail.com>
2021-05-18 16:58:40 +0300

    get settings

--- src/helpers/getConfigData.ts
+++ src/helpers/getConfigData.ts
@@ -1,23 +1,17 @@
-import { PageEvent } from "typedoc/dist/lib/output/events";
 import * as path from 'path';
 import * as fs from 'fs';
 
 export function getConfigData(prop: string, lang) {
   const fileName = 'config.json';
-  let settings;
   let config;
   let data;
 
-  if (this instanceof PageEvent) {
-      settings = this.settings;
-  }
-
-  if (settings && fs.existsSync(settings.theme)) {
-      const normalizedPath = path.join(settings.theme, fileName);
+  if (this.settings && fs.existsSync(this.settings.theme)) {
+      const normalizedPath = path.join(this.settings.theme, fileName);
       config = JSON.parse(fs.readFileSync(normalizedPath, 'utf8'));
   }
 
-  const settingOpt = settings === undefined && settings.localize === undefined ? 'en' : settings.localize;
+  const settingOpt = this.settings && this.settings.localize ? this.settings.localize : 'en';
   const getLang = lang.name ? settingOpt : lang;
   
   if (config && getLang && process.env.NODE_ENV) {
